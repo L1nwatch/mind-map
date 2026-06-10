8c075a2d23202d1ba44cc7f4ed6dc216f85ad900
L1nwatch <watch1602@gmail.com>
2026-06-10 14:15:22 +0000

    Remove mind map admin password prompt

--- web/src/api/server.js
+++ web/src/api/server.js
@@ -1,4 +1,3 @@
-const TOKEN_STORAGE_KEY = 'MIND_MAP_TOKEN'
 const DOC_ID_STORAGE_KEY = 'MIND_MAP_DOC_ID'
 
 export const isServerMode = () => {
@@ -8,24 +7,6 @@ export const isServerMode = () => {
   }
   const host = window.location.hostname
   return host === 'watch0.top' || host.endsWith('.watch0.top')
-}
-
-export const getAuthToken = () => {
-  const params = new URLSearchParams(window.location.search)
-  const queryToken = params.get('token')
-  if (queryToken) {
-    localStorage.setItem(TOKEN_STORAGE_KEY, queryToken)
-    return queryToken
-  }
-  return localStorage.getItem(TOKEN_STORAGE_KEY)
-}
-
-export const setAuthToken = token => {
-  if (!token) {
-    localStorage.removeItem(TOKEN_STORAGE_KEY)
-    return
-  }
-  localStorage.setItem(TOKEN_STORAGE_KEY, token)
 }
 
 export const getLastDocId = () => {
@@ -44,17 +25,7 @@ export const setLastDocId = docId => {
 }
 
 const apiFetch = async (path, options = {}) => {
-  let token = getAuthToken()
-  if (!token) {
-    token = window.prompt('请输入管理密码') || ''
-    if (token) {
-      setAuthToken(token)
-    }
-  }
   const headers = new Headers(options.headers || {})
-  if (token) {
-    headers.set('X-Private-Token', token)
-  }
   headers.set('Content-Type', 'application/json')
   const response = await fetch(`/api/mindmaps${path}`, {
     ...options,
